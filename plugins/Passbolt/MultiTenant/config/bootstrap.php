5854d1a8d650471abd7c452da7e4e1aa2c50680a
Kevin Muller <kevin@passbolt.com>
2018-05-08 20:26:04 +0200

    PASSBOLT-2856 fix bug migrations not loading without organization parameter

--- plugins/Passbolt/MultiTenant/config/bootstrap.php
+++ plugins/Passbolt/MultiTenant/config/bootstrap.php
@@ -53,7 +53,7 @@ $ignoreRoutes = [
     '/\/multi_tenant\/organizations/',
 ];
 $executeShell = isset($argv[1]) && !in_array($argv[1], $ignoreShells);
-$executeRoute = true;
+$executeRoute = isset($_SERVER['REQUEST_URI']);
 if (isset($_SERVER['REQUEST_URI'])) {
     $matches = array_filter($ignoreRoutes, function($regexp) {
         $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
