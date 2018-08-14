c1f56b4b08212f8451fe687805e1373400d54d80
Kevin Muller <kevin@passbolt.com>
2018-08-14 20:24:29 +0200

    add CSRF config (2)

--- plugins/Passbolt/MultiTenantAdmin/config/bootstrap.php
+++ plugins/Passbolt/MultiTenantAdmin/config/bootstrap.php
@@ -1,10 +1,4 @@
 <?php
 use Cake\Core\Configure;
 
-// Load default plugin configuration file while keeping plugin conf defined in passbolt.php.
-$original = Configure::read('passbolt.multiTenantAdmin');
 Configure::load('Passbolt/MultiTenantAdmin.config', 'default', true);
-$defaults = Configure::read('passbolt.multiTenantAdmin');
-$merged = array_merge($defaults, $original);
-Configure::write('passbolt.multiTenantAdmin', $merged);
-
