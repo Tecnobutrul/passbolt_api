71a25989db96f9d4d8bedffcc9bc4dd3470ef935
Kevin Muller <kevin@passbolt.com>
2019-07-05 16:06:25 +0530

    update environment variables add env var in docker files

--- plugins/Passbolt/MultiTenantAnalytics/config/config.php
+++ plugins/Passbolt/MultiTenantAnalytics/config/config.php
@@ -3,7 +3,7 @@ return [
     'passbolt' => [
         'multiTenantAnalytics' => [
             'entryPoint' => [
-                'url' => env('PASSBOLT_MULTI_TENANT_ANALYTICS_ENTRYPOINT_URL', 'https://my-entry-point-url.com'),
+                'url' => env('PASSBOLT_MULTI_TENANT_ANALYTICS_ENTRYPOINT_URL', 'https://europe-west1-passbolt-testing-78757.cloudfunctions.net/onOrgAnalyticsUpdate'),
                 'username' => env('PASSBOLT_MULTI_TENANT_ANALYTICS_ENTRYPOINT_USERNAME', 'username'),
                 'password' => env('PASSBOLT_MULTI_TENANT_ANALYTICS_ENTRYPOINT_PASSWORD', 'password'),
             ],
