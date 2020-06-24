dac4c3adec5c51c73ec6dfc408504990638e23b7
Cedric Alfonsi <cedric@passbolt.com>
2020-06-24 13:29:32 +0200

    Fix Cloud OpenPGP backend factory
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- src/Utility/OpenPGP/OpenPGPBackendFactory.php
+++ src/Utility/OpenPGP/OpenPGPBackendFactory.php
@@ -15,6 +15,7 @@
 namespace App\Utility\OpenPGP;
 
 use App\Utility\OpenPGP\Backends\Gnupg;
+use App\Utility\OpenPGP\Backends\Http;
 use Cake\Core\Configure;
 use Cake\Core\Exception\Exception;
 use Cake\Http\Exception\InternalErrorException;
@@ -22,6 +23,7 @@ use Cake\Http\Exception\InternalErrorException;
 class OpenPGPBackendFactory
 {
     const GNUPG = 'gnupg';
+    const HTTP = 'http';
 
     /**
      * @var OpenPGPBackend
