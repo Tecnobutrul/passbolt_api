ea4dc26bf0b620c1556666f8b415c73a23335aba
Stéphane Loegel <stephane@passbolt.com>
2023-01-06 14:20:42 +0100

    PB-21763 - Integrate SSO error feedback page into Passbolt API

---
+++ plugins/PassboltEe/Sso/src/Controller/Azure/ErrorController.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         3.9.0
+ */
+
+namespace Passbolt\Sso\Controller\Azure;
+
+use Cake\Event\EventInterface;
+use Passbolt\Sso\Controller\AbstractSsoController;
+
+/**
+ * Error Handling Controller
+ *
+ * Controller used by ExceptionRenderer to render error responses.
+ */
+class ErrorController extends AbstractSsoController
+{
+    /**
+     * @inheritDoc
+     */
+    public function beforeRender(EventInterface $event)
+    {
+        parent::beforeRender($event);
+        $this->viewBuilder()
+             ->setTemplatePath('Error')
+             ->setTemplate('error');
+    }
+}
