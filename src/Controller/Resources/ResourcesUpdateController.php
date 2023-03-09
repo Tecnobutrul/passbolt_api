71223d8aba4c02dc07d00cf8df42aeb09b25f5a9
Rodrigo Pérez <rodrigo.perez@cakedc.com>
2023-03-09 08:31:21 +0000

    PB-23321 - Upgrade CE version to CakePHP 4.4

--- src/Controller/Resources/ResourcesUpdateController.php
+++ src/Controller/Resources/ResourcesUpdateController.php
@@ -24,7 +24,7 @@ use Cake\Http\Exception\BadRequestException;
 use Cake\Validation\Validation;
 
 /**
- * @property \App\Model\Table\ResourcesTable $Resources
+ * ResourcesUpdateController Class
  */
 class ResourcesUpdateController extends AppController
 {
