c285abc9832a52eb8e89a242be667916d104d841
Cedric Alfonsi <cedric@passbolt.com>
2023-03-09 18:00:06 +0100

    PB-20535 As a community user I want to use folders - Adapt pro to the community port
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- plugins/PassboltEe/Ee/tests/TestCase/EeSolutionBootstrapperTest.php
+++ plugins/PassboltEe/Ee/tests/TestCase/EeSolutionBootstrapperTest.php
@@ -17,7 +17,6 @@ declare(strict_types=1);
 namespace Passbolt\Ee\Test\TestCase;
 
 use App\Test\Lib\SolutionBootstrapperTestCase;
-use App\Test\TestCase\BaseSolutionBootstrapperTest;
 use App\Utility\Application\FeaturePluginAwareTrait;
 use Cake\Core\Configure;
 use Cake\Core\PluginCollection;
@@ -36,6 +35,24 @@ class EeSolutionBootstrapperTest extends SolutionBootstrapperTestCase
     use IntegrationTestTrait;
 
     public const EXPECTED_EE_PLUGINS = [
+        'Passbolt/Ee',
+        'Passbolt/JwtAuthentication',
+        'Passbolt/AccountSettings',
+        'Passbolt/Import',
+        'Passbolt/InFormIntegration',
+        'Passbolt/Locale',
+        'Passbolt/Export',
+        'Passbolt/ResourceTypes',
+        'Passbolt/RememberMe',
+        'Passbolt/EmailNotificationSettings',
+        'Passbolt/EmailDigest',
+        'Passbolt/Reports',
+        'Passbolt/Mobile',
+        'Passbolt/SelfRegistration',
+        'Passbolt/PasswordGenerator',
+        'Passbolt/SmtpSettings',
+        'Passbolt/MultiFactorAuthentication',
+        'Passbolt/Log',
         'Passbolt/AuditLog',
         'Passbolt/DirectorySync',
         'Passbolt/Tags',
@@ -50,10 +67,6 @@ class EeSolutionBootstrapperTest extends SolutionBootstrapperTestCase
     {
         Configure::delete('passbolt.webInstaller.configured');
         $plugins = $this->arrangeAndGetPlugins();
-        $expectedCePlugins = $this->removePluginFromList(
-            BaseSolutionBootstrapperTest::EXPECTED_CE_PLUGINS,
-            'Passbolt/JwtAuthentication'
-        );
         $expectedPluginList = array_merge(
             [
                 'Migrations',
@@ -62,11 +75,7 @@ class EeSolutionBootstrapperTest extends SolutionBootstrapperTestCase
                 'BryanCrowe/ApiPagination',
                 'PassboltSeleniumApi',
                 'PassboltTestData',
-                'Passbolt/Ee',
-                'Passbolt/JwtAuthentication',
             ],
-            $expectedCePlugins,
-            ['Passbolt/Log'],
             self::EXPECTED_EE_PLUGINS,
             [
                 'Bake',
@@ -107,6 +116,7 @@ class EeSolutionBootstrapperTest extends SolutionBootstrapperTestCase
         $this->enableFeaturePlugin('Tags');
         $this->enableFeaturePlugin('AccountRecovery');
         $this->enableFeaturePlugin('Sso');
+        $this->enableFeaturePlugin('MfaPolicies');
         $this->enableFeaturePlugin('SsoRecover');
         // These plugins are enabled by default if not defined
         Configure::delete('passbolt.plugins.ee.enabled');
