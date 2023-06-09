e58fac6d5eda7f1b26f744f22e57ce7504439391
Diego Lendoiro <diego@passbolt.com>
2023-06-09 17:12:08 +0200

    PB-24575: added a release_notes test file

--- .gitlab-ci/scripts/bin/help_site.sh
+++ .gitlab-ci/scripts/bin/help_site.sh
@@ -12,6 +12,7 @@ PASSBOLT_HELP_DIR="passbolt_help"
 GITLAB_USER_EMAIL="contact@passbolt.com"
 GIT_CI_TOKEN_NAME=${GIT_CI_TOKEN_NAME:-gitlab-ci-token}
 ACCESS_TOKEN_NAME="help-site-bot"
+CI_COMMIT_TAG_TEST="4.0.1-test-helpsite"
 
 function create_release_notes() {
     title="$(grep name config/version.php | awk -F "'" '{print $4}')"
@@ -22,13 +23,13 @@ function create_release_notes() {
     permalink="/releases/$PASSBOLT_FLAVOUR/$(grep name config/version.php | awk -F "'" '{print $4}' | tr ' ' '_' | tr '[:upper:]' '[:lower:]')"
     date="$(date +'%Y-%m-%d')"
 
-    cat << EOF >> _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG".md
+    cat << EOF >> _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG_TEST".md
 ---
 title: $title
 slug: $slug
 layout: release
 categories: $categories
-version: $CI_COMMIT_TAG
+version: $CI_COMMIT_TAG_TEST
 product: $PASSBOLT_FLAVOUR
 song: $song
 quote: $quote
@@ -37,7 +38,7 @@ date: $date
 ---
 EOF
 
-    cat RELEASE_NOTES.md >> _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG".md
+    cat RELEASE_NOTES.md >> _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG_TEST".md
 }
 
 setup_gpg_key "$GPG_KEY_PATH" "$GPG_PASSPHRASE" "$GPG_KEY_GRIP"
@@ -46,7 +47,8 @@ setup_git_user "$GITLAB_USER_EMAIL" "$ACCESS_TOKEN_NAME"
 git clone -b master https://"$GIT_CI_TOKEN_NAME":"$CI_JOB_TOKEN"@"$HELP_SITE_REPO" "$PASSBOLT_HELP_DIR"
 cd "$PASSBOLT_HELP_DIR"
 create_release_notes
-git checkout -b release_notes_"$CI_COMMIT_TAG"
-git add _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG".md
-git commit -m ":robot: Automatically added release notes for version $CI_COMMIT_TAG $PASSBOLT_FLAVOUR"
-glab mr create -s release_notes_"$CI_COMMIT_TAG" -b master -d ":robot: Release notes for $CI_COMMIT_TAG" -t "Release notes for $CI_COMMIT_TAG" --push
+git checkout -b release_notes_"$CI_COMMIT_TAG_TEST"
+git add _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG_TEST".md
+git commit -m ":robot: Automatically added release notes for version $CI_COMMIT_TAG_TEST $PASSBOLT_FLAVOUR"
+glab mr create -s release_notes_"$CI_COMMIT_TAG_TEST" -b master -d ":robot: Release notes for $CI_COMMIT_TAG_TEST" -t "Release notes for $CI_COMMIT_TAG_TEST" --push
+bash .gitlab-ci/scripts/bin/slack-status-messages.sh ":notebook: New helpsite release notes created for $CI_COMMIT_TAG_TEST" "https://gitlab.com/passbolt/passbolt-help/-/merge_requests"
