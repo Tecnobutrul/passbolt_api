9148c24de01313bb820de6b204cf9c7175a62723
Daniel Del Rio Figueira <daniel@passbolt.com>
2023-06-10 17:45:39 +0200

    PB-24575: fix path gpg-preset-passphrase

--- .gitlab-ci/scripts/lib/git-helpers.sh
+++ .gitlab-ci/scripts/lib/git-helpers.sh
@@ -7,7 +7,7 @@ function setup_gpg_key() {
   echo "allow-preset-passphrase" > ~/.gnupg/gpg-agent.conf
   gpg-agent --allow-preset-passphrase --homedir ~/.gnupg --use-standard-socket --daemon
   gpg-connect-agent reloadagent /bye
-  /usr/libexec/gpg-preset-passphrase -c "$grip" <<< "$passphrase"
+  /usr/lib/gnupg/gpg-preset-passphrase -c "$grip" <<< "$passphrase"
   echo "Pienentry"
   gpg --pinentry-mode loopback --passphrase "$passphrase" --import "$key_path"
 }
