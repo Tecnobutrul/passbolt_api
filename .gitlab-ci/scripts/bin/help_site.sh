c84bcea67940596a30f550d7a692c0461561bf28
Daniel Del Rio Figueira <daniel@passbolt.com>
2023-06-12 15:07:32 +0200

    PB-24575: fix release notes path

--- .gitlab-ci/scripts/bin/help_site.sh
+++ .gitlab-ci/scripts/bin/help_site.sh
@@ -14,13 +14,14 @@ GIT_CI_TOKEN_NAME=${GIT_CI_TOKEN_NAME:-gitlab-ci-token}
 ACCESS_TOKEN_NAME="help-site-bot"
 HELP_SITE_REPO="gitlab.com/passbolt/passbolt-help.git"
 CI_COMMIT_TAG="v4.0.4"
+RELEASE_NOTES_PATH="../RELEASE_NOTES.md"
 
 
 function create_release_notes() {
     title="$(grep name ../config/version.php | awk -F "'" '{print $4}')"
     slug="$(grep name ../config/version.php | awk -F "'" '{print $4}' | tr ' ' '_' | tr '[:upper:]' '[:lower:]')"
     categories="releases $PASSBOLT_FLAVOUR"
-    song="$(grep 'Release song:' RELEASE_NOTES.md | awk '{print $2}')"
+    song="$(grep 'Release song:' $RELEASE_NOTES_PATH | awk '{print $2}')"
     quote="$(grep name ../config/version.php | awk -F "'" '{print $4}')"
     permalink="/releases/$PASSBOLT_FLAVOUR/$(grep name ../config/version.php | awk -F "'" '{print $4}' | tr ' ' '_' | tr '[:upper:]' '[:lower:]')"
     date="$(date +'%Y-%m-%d')"
@@ -40,7 +41,7 @@ date: $date
 ---
 EOF
 
-    cat ../RELEASE_NOTES.md >> _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG".md
+    cat $RELEASE_NOTES_PATH >> _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG".md
 }
 
 setup_gpg_key "$GPG_KEY_PATH" "$GPG_PASSPHRASE" "$GPG_KEY_GRIP"
